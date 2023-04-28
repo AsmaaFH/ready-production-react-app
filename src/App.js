b56abef692fbe281fccab8a3d5f5eb55339fe8f3
AsmaaFH <asmaakhdr24@gmail.com>
2023-04-28 16:37:21 +0300

    navbar, card components

--- src/App.js
+++ src/App.js
@@ -1,120 +1,19 @@
+import Navbar from "./components/Navbar";
+import Card from "./components/Card"
 import "./App.css";
 
+//https://via.placeholder.com/200
 function App() {
   return (
-    <div className="container text-center mt-5">
-      <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="www.ibeehire.com">
-            Navbar
-          </a>
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a
-                  class="nav-link active"
-                  aria-current="page"
-                  href="www.ibeehire.com"
-                >
-                  Homes
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="www.ibeehire.com">
-                  Link
-                </a>
-              </li>
-              <li class="nav-item dropdown">
-                <a
-                  class="nav-link dropdown-toggle"
-                  href="www.ibeehire.com"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Dropdown
-                </a>
-                <ul class="dropdown-menu">
-                  <li>
-                    <a class="dropdown-item" href="www.ibeehire.com">
-                      Action
-                    </a>
-                  </li>
-                  <li>
-                    <a class="dropdown-item" href="www.ibeehire.com">
-                      Another action
-                    </a>
-                  </li>
-                  <li>
-                    <hr class="dropdown-divider" />
-                  </li>
-                  <li>
-                    <a class="dropdown-item" href="www.ibeehire.com">
-                      Something else here
-                    </a>
-                  </li>
-                </ul>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link disabled" href="www.google.com">
-                  Disabled
-                </a>
-              </li>
-            </ul>
-            <form class="d-flex" role="search">
-              <input
-                class="form-control me-2"
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-              />
-              <button class="btn btn-outline-success" type="submit">
-                Search
-              </button>
-            </form>
-          </div>
+    <>
+      <Navbar />
+      <div className="container text-center mt-5">
+        <h1>Gallery</h1>
+        <div className="row">
+          {Array.apply(null, {length: 9}).map((_, index) => <Card key={index} />)}
         </div>
-      </nav>
-
-      <h1>Gallery</h1>
-
-      <div className="row">
-        {Array.apply(null, { length: 9 }).map(() => {
-          return (
-            <div className="col mb-5">
-              <div class="card" style={{ width: "18rem" }}>
-                <img
-                  src="https://placehold.co/200"
-                  class="card-img-top"
-                  alt="..."
-                />
-                <div class="card-body">
-                  <h5 class="card-title">Card title</h5>
-                  <p class="card-text">
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </p>
-                  <a href="www.ibeehire.com" class="btn btn-primary">
-                    Go somewhere
-                  </a>
-                </div>
-              </div>
-            </div>
-          );
-        })}
       </div>
-    </div>
+    </>
   );
 }
 
